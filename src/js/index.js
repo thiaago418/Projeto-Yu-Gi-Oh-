e7e2d3f40220b1db449723f64d1cf1c4a16e9c9d
thiaago418 <thiaago418@gmail.com>
2025-01-26 18:57:47 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+btnAvancar.addEventListener("click", function () {
+  if (cartaoAtual === cartoes.length - 1) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao(cartaoAtual);
+});
+
+btnVoltar.addEventListener("click", function () {
+  if (cartaoAtual === 0) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao(cartaoAtual);
+});
+
+function mostrarCartao(cartaoAtual) {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
+}
